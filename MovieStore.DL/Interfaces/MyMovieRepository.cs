1bef8c5a715cf21299ad6d7145d28da8da286851
Hello I'm George <georgi_1602@abv.bg>
2024-11-06 19:02:16 +0200

    adding layers

---
+++ MovieStore.DL/Interfaces/MyMovieRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieStore.Models.DTO;
+
+namespace MovieStore.DL.Interfaces
+{
+    internal interface MyMovieRepository
+    {
+        IEnumerable<Movie> GetMovies();
+    }
+}
